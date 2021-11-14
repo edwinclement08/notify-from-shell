c0188a1ad6f70083609b2b8ef2b8a46952ad2549
Edwin <edwinclement08@gmail.com>
2021-11-14 23:10:20 +0530

    add pushbullet note-type pushes

---
+++ notify-pushbullet.sh
@@ -0,0 +1,14 @@
+source ./.env
+function curl_pushbullet {
+    curl --header 'Content-Type: application/json' -u $PUSHBULLET_TOKEN: "https://api.pushbullet.com/v2/$@"
+} 
+
+
+if [[ "$#" -ne "2" ]] 
+then 
+    echo "Please give Title & Body for the message"
+    exit 3
+fi
+
+curl_pushbullet pushes  --data-binary "{\"type\": \"note\", \"title\": \"$1\", \"body\": \"$2\"}"
+
